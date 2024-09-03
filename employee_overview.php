afba8e96fa58538487fc4ef56350cb6d29d93daa
Jessy <131438501+ASoftwareDevelopmentStudentNamedJessy@users.noreply.github.com>
2024-09-03 10:32:47 +0200

    Update employee_overview.php

--- employee_overview.php
+++ employee_overview.php
@@ -2,6 +2,7 @@
 include_once "database.php";
 include_once "sessie.php";
 include_once "gebruiker.php";
+include_once "sets.php";
 
 Sessie::start(); // verzekerd dat de sessie gestart is
 
@@ -19,6 +20,23 @@ if ($userRole !== 'employee') {
     header("Location: index.php");
     exit;
 }
+
+$sql_sets = "SELECT * FROM `sets`";
+$result_speelhuys = $connectie->query($sql_sets);
+
+echo "<center><h2>sets</h2></center>";
+if ($result_speelhuys->num_rows > 0) {
+    echo "<table border='1'><tr><th>item name</th><th>description</th><th>prices</th></tr>";
+    while($row = $result_speelhuys->fetch_assoc()) {
+        $sets = new sets($row["set_name"], $row["set_description"], $row["set_price"]);
+        echo "<tr><td>" . $sets->getName() . "</td><td>" . $sets->getDescription() . "</td><td>" .  "$".$sets->getPrices() . "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Geen sets gevonden";
+}
+$connectie->close();
+
 ?>
 
 <div class="container">
